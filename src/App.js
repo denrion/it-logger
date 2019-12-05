bd3e739d37fd72cae99e0a5b596974a73d949d27
Nemanja Kuzmanovic <kuzmaneca@gmail.com>
2019-12-05 10:29:23 +0100

    Logs, LogItem, Preloader & SearchBar components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
-import React, { useEffect } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import './App.css';
+import SearchBar from './components/layout/SearchBar';
+import Logs from './components/logs/Logs';
 
 const App = () => {
   useEffect(() => {
@@ -9,7 +11,14 @@ const App = () => {
     M.AutoInit();
   }, []);
 
-  return <div className='App'>My App</div>;
+  return (
+    <Fragment>
+      <SearchBar />
+      <div className='container'>
+        <Logs />
+      </div>
+    </Fragment>
+  );
 };
 
 export default App;
