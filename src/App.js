5972e7a4c138e28cd7a754eff74358d4e8e27111
Nemanja Kuzmanovic <kuzmaneca@gmail.com>
2019-12-05 12:58:45 +0100

    TechItem, TechList & AddTech components

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ import './App.css';
 import AddBtn from './components/layout/AddBtn';
 import SearchBar from './components/layout/SearchBar';
 import AddLogModal from './components/logs/AddLogModal';
+import EditLogModal from './components/logs/EditLogModal';
 import Logs from './components/logs/Logs';
+import AddTechModal from './components/techs/AddTechModal';
+import TechListModal from './components/techs/TechListModal';
 
 const App = () => {
   useEffect(() => {
@@ -19,7 +22,10 @@ const App = () => {
       <div className='container'>
         <AddBtn />
         <AddLogModal />
+        <EditLogModal />
+        <AddTechModal />
         <Logs />
+        <TechListModal />
       </div>
     </Fragment>
   );
