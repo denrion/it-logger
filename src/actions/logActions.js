f3bdcfaacce48497602d90853a4ea10d2f4d09ce
Nemanja Kuzmanovic <kuzmaneca@gmail.com>
2019-12-09 15:42:16 +0100

    LogsReducer, Actions & Types

---
+++ src/actions/logActions.js
@@ -0,0 +1,19 @@
+import { GET_LOGS, LOGS_ERROR, SET_LOADING } from './types';
+
+export const getLogs = () => async dispatch => {
+  try {
+    setLoading();
+
+    const res = await fetch('/logs');
+    const data = res.json();
+
+    dispatch({ type: GET_LOGS, payload: data });
+  } catch (err) {
+    dispatch({ type: LOGS_ERROR, payload: err.response.data });
+  }
+};
+
+// Set loading to true
+export const setLoading = () => {
+  return { type: SET_LOADING };
+};
