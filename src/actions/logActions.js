c362cfd8b4ccb3a87439fe29dfdcb54ae5c06d95
Nemanja Kuzmanovic <kuzmaneca@gmail.com>
2019-12-09 15:54:08 +0100

    Connect Redux to Logs Component

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -1,14 +1,16 @@
 import { GET_LOGS, LOGS_ERROR, SET_LOADING } from './types';
 
+// Get logs from server
 export const getLogs = () => async dispatch => {
   try {
     setLoading();
 
     const res = await fetch('/logs');
-    const data = res.json();
+    const data = await res.json();
 
     dispatch({ type: GET_LOGS, payload: data });
   } catch (err) {
+    console.log(err);
     dispatch({ type: LOGS_ERROR, payload: err.response.data });
   }
 };
