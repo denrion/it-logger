a1f3fd94ccc53bbe51a27508c3e26a873222892e
Nemanja Kuzmanovic <kuzmaneca@gmail.com>
2019-12-09 15:25:03 +0100

    Redux Store & Provider Setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
 import React, { Fragment, useEffect } from 'react';
+import { Provider } from 'react-redux';
 import './App.css';
 import AddBtn from './components/layout/AddBtn';
 import SearchBar from './components/layout/SearchBar';
@@ -9,6 +10,7 @@ import EditLogModal from './components/logs/EditLogModal';
 import Logs from './components/logs/Logs';
 import AddTechModal from './components/techs/AddTechModal';
 import TechListModal from './components/techs/TechListModal';
+import store from './store';
 
 const App = () => {
   useEffect(() => {
@@ -17,17 +19,19 @@ const App = () => {
   }, []);
 
   return (
-    <Fragment>
-      <SearchBar />
-      <div className='container'>
-        <AddBtn />
-        <AddLogModal />
-        <EditLogModal />
-        <AddTechModal />
-        <Logs />
-        <TechListModal />
-      </div>
-    </Fragment>
+    <Provider store={store}>
+      <Fragment>
+        <SearchBar />
+        <div className='container'>
+          <AddBtn />
+          <AddLogModal />
+          <EditLogModal />
+          <AddTechModal />
+          <Logs />
+          <TechListModal />
+        </div>
+      </Fragment>
+    </Provider>
   );
 };
 
